9c175e0aac10a5dc2fbf2dd6c676cfc40653bfcc
Slava37 <grante199213@gmail.com>
2018-02-09 12:18:51 +0300

    Исправление ошибок

--- js/map.js
+++ js/map.js
@@ -13,14 +13,14 @@ var TITLES = [
 var TYPES = ['flat', 'house', 'bungalo'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
-var FESTURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var WIDTH_PIN = 40;
-var amountHouses = 8;
-var maxGuests = 20;
+var AMOUNT_HOUSES = 8;
+var MAX_GUESTS = 20;
 
 var userDialog = document.querySelector('.map');
 userDialog.classList.remove('map--faded');
@@ -29,7 +29,7 @@ var buttonTemplate = document.querySelector('.map__pin');
 var mapCardTemplate = document.querySelector('template').content.querySelector('article.map__card');
 var mapFilterContainer = document.querySelector('.map__filters-container');
 
-var newHouses = genHouses(amountHouses);
+var newHouses = genHouses(AMOUNT_HOUSES);
 userDialog.querySelector('.map__pins').appendChild(makeFragmentPins(newHouses));
 userDialog.insertBefore(makeFragmentMapCards(newHouses), mapFilterContainer);
 
@@ -38,21 +38,6 @@ userDialog.insertBefore(makeFragmentMapCards(newHouses), mapFilterContainer);
  * Возаращает новую метку, созданный на основе данных параметра (объекта).
  */
 function createButtonsPin(house) {
-  /*
-  var buttonElement = document.createElement('button');
-  buttonElement.style.left = 'left: ' + house.location.x + 'px;';
-  buttonElement.style.top = 'top: ' + (house.location.y) + 'px;';
-  buttonElement.className = 'map__pin';
-
-  var innImg = document.createElement('img');
-  innImg.src = house.author.avatar;
-  innImg.width = '40';
-  innImg.height = '40';
-  innImg.draggable = 'false';
-
-  buttonElement.appendChild(innImg);
-  return buttonElement;
-  */
   var pinElement = buttonTemplate.cloneNode(true);
   pinElement.querySelector('img').src = house.author.avatar;
   pinElement.style.left = house.location.x + 'px';
@@ -78,7 +63,7 @@ function createMapCard(house) {
   var mapCardElement = mapCardTemplate.cloneNode(true);
   mapCardElement.querySelector('h3').textContent = house.offer.title;
   mapCardElement.querySelector('small').textContent = house.offer.address;
-  mapCardElement.querySelector('.popup__price').innerHTML = house.offer.price + '&#x20bd;/ночь';
+  mapCardElement.querySelector('.popup__price').textContent = house.offer.price + '\u20BD/ночь';
   switch (house.offer.type) {
     case 'flat':
       mapCardElement.querySelector('h4').textContent = 'Квартира';
@@ -99,7 +84,7 @@ function createMapCard(house) {
     for (var j = 0; j < features.length; j++) {
       var feature = 'feature feature--' + features[j];
       if (personFeatures[i].className === feature) {
-        personFeatures[i].innerHTML = features[j];
+        personFeatures[i].textContent = features[j];
       }
     }
   }
@@ -107,26 +92,29 @@ function createMapCard(house) {
   mapCardElement.getElementsByTagName('p')[4].textContent = house.offer.description;
 
   // Задаем картинки жилища.
-  var photosLiTemplate = document.querySelector('template').content.querySelector('.popup__pictures li');
   var photosList = document.querySelector('template').content.querySelector('.popup__pictures');
+  var photosLiTemplate = photosList.querySelector('li');
 
   var photoFragment = document.createDocumentFragment();
+
+  var photoTemplateImg = photosLiTemplate.querySelector('img');
+  var photoElementImg;
   for (i = 0; i < house.offer.photos.length; i++) {
     if (i === 0) {
-      photosLiTemplate.querySelector('img').src = house.offer.photos[i];
-      photosLiTemplate.querySelector('img').width = '50';
-      photosLiTemplate.querySelector('img').height = '50';
+      photoTemplateImg.src = house.offer.photos[i];
+      photoTemplateImg.width = '50';
+      photoTemplateImg.height = '50';
       continue;
     }
     var photoElement = photosLiTemplate.cloneNode(true);
-    photoElement.querySelector('img').src = house.offer.photos[i];
-    photoElement.querySelector('img').width = '50'; // Не понял насчет ширины и высоты, нужно подобрать размер?
-    photoElement.querySelector('img').height = '50';
+    photoElementImg = photoElement.querySelector('img');
+    photoElementImg.src = house.offer.photos[i];
+    photoElementImg.width = '50';
+    photoElementImg.height = '50';
 
     photoFragment.appendChild(photoElement);
   }
   photosList.appendChild(photoFragment);
-
   // Меняем картинку аватара.
   var avatar = document.querySelector('template').content.querySelector('.popup__avatar');
   avatar.src = house.author.avatar;
@@ -152,7 +140,7 @@ function genHouses(num) {
   for (var i = 0; i < num; i++) {
     var x = getRandomNumber(300, 900);
     var y = getRandomNumber(150, 500);
-    houses[i] =
+    houses.push(
         {
           author: {
             avatar: avatars[i]
@@ -163,10 +151,10 @@ function genHouses(num) {
             price: getRandomNumber(1000, 1000000),
             type: getRandomElement(TYPES),
             rooms: getRandomNumber(1, 5),
-            guests: getRandomNumber(1, maxGuests),
+            guests: getRandomNumber(1, MAX_GUESTS),
             checkin: getRandomElement(CHECKIN),
             checkout: getRandomElement(CHECKOUT),
-            features: getRandomCollection(FESTURES, getRandomNumber(0, FESTURES.length)),
+            features: getRandomCollection(FEATURES, getRandomNumber(0, FEATURES.length)),
             description: '',
             photos: getRandomCollection(PHOTOS, PHOTOS.length)
           },
@@ -174,7 +162,7 @@ function genHouses(num) {
             x: x,
             y: y
           }
-        };
+        });
   }
   return houses;
 }
@@ -204,8 +192,7 @@ function getRandomNumber(min, max) {
    * индекс массива, и случайный массив.
    */
 function getRandomIndex(arr) {
-  var value = getRandomNumber(0, arr.length - 1);
-  return value;
+  return getRandomNumber(0, arr.length - 1);
 }
 
 function getRandomElement(arr) {
@@ -216,11 +203,10 @@ function getRandomElement(arr) {
 function getRandomCollection(arr, N) {
   var copyArr = arr.slice();
   var resultArr = [];
-  var element;
+  var index;
   for (var i = 0; i < N; i++) {
-    element = getRandomIndex(copyArr);
-    resultArr.splice(i, 0, copyArr[element]);
-    copyArr.splice(element, 1);
+    index = getRandomIndex(copyArr);
+    resultArr.push(copyArr.splice(index, 1)[0]);
   }
   return resultArr;
 }
