4ff20967f7c992632ec21deb2446c3fa75ebf327
Slava37 <grante199213@gmail.com>
2018-02-11 17:01:44 +0300

    #13 Личный проект: подробности

--- js/map.js
+++ js/map.js
@@ -19,20 +19,13 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var WIDTH_PIN = 40;
+var HEIGTH_PIN = 44;
+var HEIGTH_TIP_OF_PIN = 18; // border-top-width: 22px - 4px
 var AMOUNT_HOUSES = 8;
 var MAX_GUESTS = 20;
 
-var userDialog = document.querySelector('.map');
-userDialog.classList.remove('map--faded');
-
-var buttonTemplate = document.querySelector('.map__pin');
-var mapCardTemplate = document.querySelector('template').content.querySelector('article.map__card');
-var mapFilterContainer = document.querySelector('.map__filters-container');
 
 var newHouses = genHouses(AMOUNT_HOUSES);
-userDialog.querySelector('.map__pins').appendChild(makeFragmentPins(newHouses));
-userDialog.insertBefore(makeFragmentMapCards(newHouses), mapFilterContainer);
-
 
 /*
  * Возаращает новую метку, созданный на основе данных параметра (объекта).
@@ -40,9 +33,17 @@ userDialog.insertBefore(makeFragmentMapCards(newHouses), mapFilterContainer);
 function createButtonsPin(house) {
   var pinElement = buttonTemplate.cloneNode(true);
   pinElement.querySelector('img').src = house.author.avatar;
+
   pinElement.style.left = house.location.x + 'px';
+
   pinElement.style.top = (house.location.y - WIDTH_PIN) + 'px';
 
+  // Добавляем обработчик и создание карточки для дома.
+  pinElement.addEventListener('click', function () {
+    var fragmentCard = document.createDocumentFragment();
+    fragmentCard.appendChild(createMapCard(house));
+    userDialog.insertBefore(fragmentCard, mapFilterContainer);
+  });
   return pinElement;
 }
 /*
@@ -53,7 +54,6 @@ function makeFragmentPins(houses) {
   for (var i = 0; i < houses.length; i++) {
     fragment.appendChild(createButtonsPin(houses[i]));
   }
-
   return fragment;
 }
 /*
@@ -61,8 +61,13 @@ function makeFragmentPins(houses) {
  */
 function createMapCard(house) {
   var mapCardElement = mapCardTemplate.cloneNode(true);
+
+  mapCardElement.dataset.title = house.offer.title;
   mapCardElement.querySelector('h3').textContent = house.offer.title;
+
+  mapCardElement.dataset.address = house.offer.address;
   mapCardElement.querySelector('small').textContent = house.offer.address;
+
   mapCardElement.querySelector('.popup__price').textContent = house.offer.price + '\u20BD/ночь';
   switch (house.offer.type) {
     case 'flat':
@@ -75,6 +80,7 @@ function createMapCard(house) {
       mapCardElement.querySelector('h4').textContent = 'Дом';
       break;
   }
+  mapCardElement.dataset.price = house.offer.title;
   mapCardElement.getElementsByTagName('p')[2].textContent = house.offer.rooms + ' комнаты для ' + house.offer.guests + ' гостей';
   mapCardElement.getElementsByTagName('p')[3].textContent = 'Заезд после ' + house.offer.checkin + ', выезд до ' + house.offer.checkout;
   var features = house.offer.features;
@@ -115,19 +121,12 @@ function createMapCard(house) {
     photoFragment.appendChild(photoElement);
   }
   photosList.appendChild(photoFragment);
+
   // Меняем картинку аватара.
   var avatar = document.querySelector('template').content.querySelector('.popup__avatar');
   avatar.src = house.author.avatar;
-  return mapCardElement;
-}
 
-/*
-   * Возвращает фрагмент, созданный из массива обьявлений.
-   */
-function makeFragmentMapCards(houses) {
-  var fragment = document.createDocumentFragment();
-  fragment.appendChild(createMapCard(houses[0]));
-  return fragment;
+  return mapCardElement;
 }
 
 /*
@@ -210,3 +209,31 @@ function getRandomCollection(arr, N) {
   }
   return resultArr;
 }
+
+// Обработка отпускания кнопки мыши при перетаскивании, появляется активное окно.
+
+var mainPin = document.querySelector('.map__pin--main');
+var userDialog = document.querySelector('.map');
+var fieldSets = document.querySelector('.notice__form').querySelectorAll('fieldset');
+
+// Заполнение поля адреса координатами стартовой позиции метки.
+
+var address = document.getElementById('address');
+var startPositionPin = (mainPin.offsetLeft + WIDTH_PIN / 2) + ', ' + (mainPin.offsetTop + HEIGTH_PIN / 2);
+address.value = startPositionPin;
+
+mainPin.addEventListener('mouseup', function () {
+  userDialog.classList.remove('map--faded'); // Сняли класс у активной карты.
+  fieldSets.forEach(function (value) {
+    value.removeAttribute('disabled'); // Сняли disabled у всех тегов fieldset.address.attributes.setNamedItem('disabled');
+  });
+  address.setAttribute('disabled', true); // Поле адреса всегда недоступно.
+  address.value = (mainPin.offsetLeft + WIDTH_PIN / 2) + ', ' + (mainPin.offsetTop + HEIGTH_TIP_OF_PIN + HEIGTH_PIN / 2); // Устанавливаем координаты адреса, на конце метки.
+
+  userDialog.querySelector('.map__pins').appendChild(makeFragmentPins(newHouses)); // Поставили метки обьявлений.
+});
+
+// Отрисовываем карту при запуске страницы.
+var buttonTemplate = document.querySelector('.map__pin');
+var mapCardTemplate = document.querySelector('template').content.querySelector('article.map__card');
+var mapFilterContainer = document.querySelector('.map__filters-container');
